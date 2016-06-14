328fe68f7bc47916b10b1daa5bb6cd3f9157ea1b
sitev <admin@sitev.ru>
2016-06-14 12:19:57 +0500

    Second commit

--- fc.cpp
+++ fc.cpp
@@ -1,7 +1,15 @@
 #include "stdafx.h"
 
+#include "cj.h"
+#include "cjUI.h"
+#include "cjSFML.h"
+
+using namespace cj;
+
 int main()
 {
+	RenderWindow *window = new RenderWindow(nullptr, 960, 640, "Flappy Cow");
+
 	return 0;
 }
 
